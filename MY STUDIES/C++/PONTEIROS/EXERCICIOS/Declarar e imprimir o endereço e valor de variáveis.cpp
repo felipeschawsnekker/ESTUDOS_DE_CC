5ffbbce4c83bcf78389b67ae9f0a4476ca7855d5
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2026-02-10 19:47:07 -0300

    Update Declarar e imprimir o endereço e valor de variáveis.cpp

--- MY STUDIES/C++/PONTEIROS/EXERCICIOS/Declarar e imprimir o endereço e valor de variáveis.cpp
+++ MY STUDIES/C++/PONTEIROS/EXERCICIOS/Declarar e imprimir o endereço e valor de variáveis.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int a,b,c;
+    int *ponteiro; //declaracao de uma variável do tipo ponteiro
+    a=1,b=2,c=3;
+    ponteiro = &a;
+    cout<<"O endereço do ponteiro é: "<<ponteiro<<", o valor do ponteiro é: "<<*ponteiro;
+    system("PAUSE");
+    return 0;
+}
